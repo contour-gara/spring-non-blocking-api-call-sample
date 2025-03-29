d671fabca382eb916feec907c1f864f07957b42e
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-03-29 19:26:36 +0900

    ログを追加

--- request-server/src/main/java/org/contourgara/ThreadSleepController.java
+++ request-server/src/main/java/org/contourgara/ThreadSleepController.java
@@ -1,14 +1,17 @@
 package org.contourgara;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class ThreadSleepController {
     @GetMapping("/threadSleep")
     Map<String, String> threadSleep(@RequestHeader("X-Request-Id") String requestId) throws InterruptedException {
+        log.info("Request ID: {}", requestId);
         Thread.sleep(10000);
         return Map.of("requestId", requestId);
     }
