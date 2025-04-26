7d8b1cba4a6c63cdd13ca3919931e6f6d1d0ccd8
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-04-26 18:20:52 +0900

    WebClient の例外処理を実装

--- call-api-app/src/main/java/org/contourgara/infrastructure/WebClientThreadSleepClient.java
+++ call-api-app/src/main/java/org/contourgara/infrastructure/WebClientThreadSleepClient.java
@@ -1,15 +1,19 @@
 package org.contourgara.infrastructure;
 
+import lombok.extern.slf4j.Slf4j;
 import org.contourgara.common.AppConfig;
 import org.contourgara.common.RequestId;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.IntStream;
 
 @Repository("web-client")
+@Slf4j
 public class WebClientThreadSleepClient implements ThreadSleepClient {
     private final RequestId requestId;
     private final WebClient webClient;
@@ -31,7 +35,13 @@ public class WebClientThreadSleepClient implements ThreadSleepClient {
                                         .uri("/thread-sleep")
                                         .header("X-Request-Id", "%s-%s".formatted(requestId.getRequestId(), i))
                                         .retrieve()
-                                        .bodyToMono(String.class))
+                                        .bodyToMono(String.class)
+                                        .doOnRequest($ -> log.info("{}: リクエスト実行", "%s-%s".formatted(requestId.getRequestId(), i)))
+                                        .doOnSuccess($ -> log.info("{}: リクエスト成功", "%s-%s".formatted(requestId.getRequestId(), i)))
+                                        .doOnError($ -> log.info("{}: リクエスト失敗", "%s-%s".formatted(requestId.getRequestId(), i)))
+                                        .retryWhen(Retry.fixedDelay(4, Duration.ofSeconds(3L))
+                                                .onRetryExhaustedThrow((spec, signal) -> new RuntimeException(signal.failure()))
+                                                .doAfterRetry(signal -> log.warn("{}: リトライ {} 回目", "%s-%s".formatted(requestId.getRequestId(), i), signal.totalRetries() + 1))))
                 )
         )
                 .collectList()
